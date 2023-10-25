1e9c6f80065c2938c828c2cb789e187f09bf48ec
UP <akarealup@gmail.com>
2023-10-25 21:53:15 +0800

    add acwing 104

---
+++ chapter6/acwing104.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func _debug() {
+	const eof = 0
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+	_i, _n, buf := 0, 0, make([]byte, 1<<12) // 4KB
+
+	// 读一个字符
+	rc := func() byte {
+		if _i == _n {
+			_n, _ = os.Stdin.Read(buf)
+			if _n == 0 { // EOF
+				return eof
+			}
+			_i = 0
+		}
+		b := buf[_i]
+		_i++
+		return b
+	}
+
+	// 读一个整数，支持负数
+	ri := func() (x int) {
+		neg := false
+		b := rc()
+		for ; '0' > b || b > '9'; b = rc() {
+			// 某些多组数据的题目，不告诉有多少组数据，那么需要额外判断是否读到了 EOF
+			if b == eof {
+				return
+			}
+			if b == '-' {
+				neg = true
+			}
+		}
+		for ; '0' <= b && b <= '9'; b = rc() {
+			x = x*10 + int(b&15)
+		}
+		if neg {
+			return -x
+		}
+		return
+	}
+	_ = []interface{}{rc, ri}
+
+	// acwing 104
+	n := ri()
+
+	nums := make([]int, n)
+	for i := range nums {
+		nums[i] = ri()
+	}
+
+	sort.Ints(nums)
+
+	sum := 0
+	for i, j := 0, len(nums)-1; i < j; i, j = i+1, j-1 {
+		sum += nums[j] - nums[i]
+	}
+	fmt.Fprintln(out, sum)
+}
+
+func main() {
+	_debug()
+}
